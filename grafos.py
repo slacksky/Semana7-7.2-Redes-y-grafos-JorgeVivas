2d692d72c29132f79ec4329d4cc58939efc4d070
Jorge L. Vivas C <slacksky@users.noreply.github.com>
2021-06-14 13:41:13 +0000

    Initial commit

---
+++ grafos.py
@@ -0,0 +1,20 @@
+""" Modulo para la definición de funciones
+    para crear y graficar grafos
+    Oscar Franco-Bedoya
+    Junio 2-2021 """
+
+#======================================================================
+#          E S P A C I O    D E    T R A B A J O     A L U M N O
+# ====================================================================
+
+#----------Definición de Funciones (Dividir)------------
+def nodos_ejes():
+  return "No implementado"
+
+def actores_peliculas():
+  return "No implementado"
+
+def desde_cero():
+  return "No implementado"
+
+  
